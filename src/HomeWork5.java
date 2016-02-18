fbc26aaf207cbdf043a701ff9403165a85269c74
Александр <britishop@gmail.com>
2016-02-18 15:29:07 +0200

    Lesson3

--- src/HomeWork5.java
+++ src/HomeWork5.java
@@ -10,14 +10,14 @@ public class HomeWork5 {
 
         int a;
         int sum = 0;
-        int[] Array = {3, 5, 1, 9, 56, 33, 14, -6, 0};
+        int[] array = {3, 5, 1, 9, 56, 33, 14, -6, 0};
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("How much elements do you want to add?");
 
         a = scanner.nextInt();
         for (int i = 0; i < a; i++) {
-            sum += Array[i];
+            sum += array[i];
         }
         System.out.println("Sum of the first " + a + " elements is " + sum);
     }
